31890ac54e13e3daff2d3f0a5482fcb2f56cca30
Adrien Frischknecht <adrienfrischknecht@MacBook-Pro-de-Adrien.local>
2023-12-09 13:02:35 +0100

    Separation of responsive CSS, modification in JS to improve contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,31 +3,48 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const modal = document.querySelector(".modal");
   const body = document.querySelector("body");
+  const form = document.querySelector("#contact-form");
 
   document.querySelector(".button-sign-up").addEventListener("click", () => {
     modal.classList.remove("hidden");
     body.classList.add("disable");
   });
 
-  document
-    .querySelector("#contact-form")
-    .addEventListener("submit", async (event) => {
-      event.preventDefault();
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-      const data = {
-        firstname: document.querySelector("#firstname").value,
-        lastname: document.querySelector("#lastname").value,
-        email: document.querySelector("#email").value,
-        message: document.querySelector("#message").value,
-      };
+    const data = {
+      firstname: document.querySelector("#firstname").value,
+      lastname: document.querySelector("#lastname").value,
+      email: document.querySelector("#email").value,
+      message: document.querySelector("#message").value,
+    };
 
-      console.log(data);
+    console.log(data);
 
-      const response = await axios.post("http://localhost:3000/contact-form");
-      console.log("Response =>", response);
-    });
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/contact-form",
+        data
+      );
+      if (response.status === 200) {
+        alert("Votre formulaire a bien été envoyé");
+      }
+      form.reset();
+    } catch (error) {
+      if (error.response?.data.message === "Missing parameters") {
+        alert("Veuillez remplir tous les champs du formulaire");
+      } else {
+        alert("Une erreur est survenue");
+        form.reset();
+      }
+    }
+
+    console.log("Response =>", response);
+  });
 
   document.querySelector(".icon-times").addEventListener("click", () => {
+    form.reset();
     modal.classList.add("hidden");
     body.classList.remove("disable");
   });
